45238794dde977f973064d0d5e24d1efcff9fdfa
Naomi Martin <naomi.martin@alleninstitute.org>
2024-05-23 09:49:54 -0700

    update setup.py to include ilastik models?

--- setup.py
+++ setup.py
@@ -19,6 +19,10 @@ setup(
     license = "LICENSE",
     packages=find_packages(where="."),
     include_package_data=True,
+	package_data={
+        # Ensure the package name is correct and the path to the files is correct
+        'merquaco': ['ilastik_models/*.ilp'],
+    },
     install_requires=requirements,
     # entry_points={
     #     "console_scripts": [
