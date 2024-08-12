d0dfb6e56d95e3a42b66f878abd3da05c1315bfe
polsen99 <paul.olsen@alleninstitute.org>
2024-08-12 10:41:02 -0700

    read transcripts, get FOVs

--- merquaco/experiment.py
+++ merquaco/experiment.py
@@ -25,7 +25,149 @@ metrics_dict_keys = ["filtered_transcripts_count", "transcript_density_um2", "tr
                      "merquaco_version"]
 
 
-class Experiment:
+@staticmethod
+def read_transcripts(transcripts_path: Union[str, Path]) -> pd.DataFrame:
+    """
+    Reads and returns transcripts table dataframe
+
+    Parameters
+    ----------
+    transcripts_path : str or Path
+        Path to transcripts table
+
+    Returns
+    -------
+    transcripts : pd.DataFrame
+        Transcripts table
+
+    Raises
+    ------
+    FileNotFoundError
+        If file not found at given path
+    """
+    # Newer xenium experiments
+    if transcripts_path.endswith('parquet'):
+        try:
+            transcripts = pd.read_parquet(transcripts_path)
+            return transcripts
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f'transcripts parquet file not found at {transcripts_path}'
+                                    f'error: {e}')
+    # MERSCOPE experiments / older xenium experiments
+    else:
+        try:
+            transcripts = pd.read_csv(transcripts_path)
+            return transcripts
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f'transcripts csv not found at {transcripts_path}'
+                                    f'error: {e}')
+
+
+@staticmethod
+def find_fovs(transcripts: pd.DataFrame) -> pd.DataFrame:
+    """
+    Groups transcript table by FOV and stores coordinate and count information
+
+    Parameters
+    ----------
+    transcripts : pd.DataFrame
+        Transcripts dataframe
+
+    Returns
+    -------
+    fovs : pd.DataFrame
+        FOVs dataframe
+    """
+    # Get min/max coords
+    fovs = transcripts[['global_x', 'global_y', 'fov']].groupby('fov').min()
+    fovs.rename(columns={'global_x': 'x_min', 'global_y': 'y_min'}, inplace=True)
+    fovs[['x_max', 'y_max']] = transcripts[['global_x', 'global_y', 'fov']].groupby('fov').max()
+
+    # Width/height
+    fovs['width'] = fovs['x_max'] - fovs['x_min']
+    fovs['height'] = fovs['y_max'] = fovs['y_min']
+
+    # Centroids
+    fovs['center_x'] = (fovs['x_max'] - fovs['x_min']) / 2 + fovs['x_min']
+    fovs['center_y'] = (fovs['y_max'] - fovs['y_min']) / 2 + fovs['y_min']
+
+    # Transcript counts per FOV
+    fovs['transcript_counts'] = transcripts.groupby('fov').size()
+
+    # Counts per z-plane
+    planes = transcripts['global_z'].nunique()
+    for i in range(planes):
+        fovs[f'z{i}_count'] = transcripts[transcripts['global_z'] == i].groupby('fov').size()
+
+    # Counts per gene
+    counts_per_gene = transcripts.groupby(['fov'])['gene'].value_counts().unstack(fill_value=0)
+    fovs = fovs.merge(counts_per_gene, left_index=True, right_index=True, how='left')
+
+    return fovs
+
+
+@staticmethod
+def get_fov_neighbors(fovs: pd.DataFrame) -> pd.DataFrame:
+    """
+    Get cardinal neighbors for each FOV using grid coordinates
+
+    Parameters
+    ----------
+    fovs : pd.DataFrame
+        FOVs dataframe
+
+    Returns
+    -------
+    fovs : pd.DataFrame
+        Updated FOVs dataframe with neighbor locations
+    """
+    max_width = np.max(fovs['width'])
+    max_height = np.max(fovs['height'])
+    
+    centers_array = np.array(fovs[['center_x', 'center_y']])
+
+    neighbors = [[] for i in range(len(fovs))]
+
+    for i in range(len(fovs)):
+        # Get target FOV
+        fov = fovs.index[i]
+        # Find euclidian distance between grid coords of each FOV and all other FOVs
+        fov_center = np.broadcast_to(centers_array[i], (len(centers_array), 2))
+        euclidian_distances = np.argsort(np.linalg.norm(fov_center - centers_array, axis=1))
+
+        # Neighbor above
+        above_fovs = np.unique(np.where((centers_array[:, 1] > fovs.loc[fov, 'y_max']) &
+                                        (abs(centers_array[:, 0] - centers_array[i, 0]) <= max_height / 2) &
+                                        (abs(centers_array[:, 1] - centers_array[:, 1]) <= max_height * 1.5))[0])
+        if len(above_fovs) > 0:
+            neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, above_fovs)][0]])
+
+        # Neighbor below
+        below_fovs = np.unique(np.where((centers_array[:, 1] < fovs.loc[fov, 'y_min']) &
+                                        (abs(centers_array[:, 0] - centers_array[i, 0]) <= max_height / 2) &
+                                        (abs(centers_array[:, 1] - centers_array[:, 1]) <= max_height * 1.5))[0])
+        if len(below_fovs) > 0:
+            neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, below_fovs)][0]])
+
+        # Neighbor right
+        right_fovs = np.unique(np.where((centers_array[:, 0] > fovs.loc[fov, 'x_max']) &
+                                            (abs(centers_array[:, 1] - centers_array[i, 1]) <= max_width / 2) &
+                                            (abs(centers_array[:, 0] - centers_array[i, 0]) <= max_width * 1.5))[0])
+        if len(right_fovs) > 0:
+            neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, right_fovs)][0]])
+
+        # Neighbor left
+        left_fovs = np.unique(np.where((centers_array[:, 0] > fovs.loc[fov, 'x_min']) &
+                                        (abs(centers_array[:, 1] - centers_array[i, 1]) <= max_width / 2) &
+                                        (abs(centers_array[:, 0] - centers_array[i, 0]) <= max_width * 1.5))[0])
+        if len(left_fovs) > 0:
+            neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, left_fovs)][0]])
+
+    fovs['neighbors'] = neighbors
+    return fovs
+
+
+class MerscopeExperiment:
 
     def __init__(self,
                  transcripts_input: Union[pd.DataFrame, str, Path],
@@ -148,33 +290,6 @@ class Experiment:
                 self.transcripts_mask = data_processing.process_path(self.transcripts_mask_path)
 
     @staticmethod
-    def read_transcripts(transcripts_path: Union[str, Path]) -> pd.DataFrame:
-        """
-        Reads and returns transcripts table dataframe
-
-        Parameters
-        ----------
-        transcripts_path : str or Path
-            Path to detected_transcripts.csv file
-
-        Returns
-        -------
-        transcripts : pd.DataFrame
-            Transcripts table
-
-        Raises
-        ------
-        FileNotFoundError
-            If file not found at given path
-        """
-        try:
-            transcripts = pd.read_csv(transcripts_path)
-            return transcripts
-        except FileNotFoundError as e:
-            raise FileNotFoundError(f'detected_transcripts.csv not found at {transcripts_path}'
-                                    f'Error: {e}')
-
-    @staticmethod
     def remove_blanks(transcripts: pd.DataFrame) -> pd.DataFrame:
         """
         Filters transcripts DataFrame to remove 'Blank' codewords
@@ -240,110 +355,6 @@ class Experiment:
         return transcripts
 
     @staticmethod
-    def find_fovs(transcripts: pd.DataFrame) -> pd.DataFrame:
-        """
-        Group transcripts by FOV and stores per-FOV information, including coordinates and transcript counts
-
-        Parameters
-        ----------
-        transcripts : pd.DataFrame
-            Transcripts DataFrame
-
-        Returns
-        -------
-        fovs : pd.DataFrame
-            FOVs DataFrame
-        """
-        # Get min/max coords
-        fovs = transcripts[['global_x', 'global_y', 'fov']].groupby('fov').min()
-        fovs.rename(columns={'global_x': 'x_min', 'global_y': 'y_min'}, inplace=True)
-        fovs[['x_max', 'y_max']] = transcripts[['global_x', 'global_y', 'fov']].groupby('fov').max()
-
-        # Width/height
-        fovs['width'] = fovs['x_max'] - fovs['x_min']
-        fovs['height'] = fovs['y_max'] - fovs['y_min']
-
-        # Centroids
-        fovs['center_x'] = (fovs['x_max'] - fovs['x_min']) / 2 + fovs['x_min']
-        fovs['center_y'] = (fovs['y_max'] - fovs['y_min']) / 2 + fovs['y_min']
-
-        # Add transcript counts per FOV
-        num_planes = transcripts['global_z'].nunique()
-        fovs['transcripts_count'] = transcripts.groupby('fov').size()
-
-        # Add counts per z-plane per FOV
-        for i in range(num_planes):
-            fovs[f'z{i}_count'] = transcripts[transcripts['global_x'] == i].groupby('fov').size()
-
-        # Add z-ratio per z plane
-        fovs['z_ratio'] = fovs[f'z{num_planes - 1}_count'] / fovs['z0_count']
-
-        return fovs
-
-    @staticmethod
-    def get_fov_neighbors(fovs: pd.DataFrame) -> pd.DataFrame:
-        """
-        Find neighbors for each FOV using grid coordinates
-
-        Uses Euclidean distance between FOV centers to identify closest FOVs in cardinal direction
-
-        Parameters
-        ----------
-        fovs : pd.DataFrame
-            FOVs dataframe containing min/max/width/height/center coords
-
-        Returns
-        -------
-        fovs : pd.DataFrame
-            Modified to contain neighbor information
-        """
-        # Size of grid square
-        max_width = np.max(fovs['width'])
-        max_height = np.max(fovs['height'])
-        grid_sq_size = max((max_width, max_height))
-
-        centers_array = np.array(fovs[['center_x', 'center_y']])
-
-        neighbors = [[] for i in range(len(fovs))]
-
-        for i in range(len(fovs)):
-            fov = fovs.index[i]
-            # Find euclidean distance between grid coords of each FOV and all other FOVs
-            fov_center = np.broadcast_to(centers_array[i], (len(centers_array), 2))
-            euclidian_distances = np.argsort(np.linalg.norm(fov_center - centers_array, axis=1))
-
-            # Neighbor above
-            above_fovs = np.unique(np.where((centers_array[:, 1] > fovs.loc[fov, 'y_max']) &
-                                            (abs(centers_array[:, 0] - centers_array[i, 0]) <= grid_sq_size / 2) &
-                                            (abs(centers_array[:, 1] - centers_array[i, 1]) <= grid_sq_size * 1.5))[0])
-            if len(above_fovs) > 0:
-                neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, above_fovs)][0]])
-
-            # Neighbor below
-            below_fovs = np.unique(np.where((centers_array[:, 1] > fovs.loc[fov, 'y_min']) &
-                                            (abs(centers_array[:, 0] - centers_array[i, 0]) <= grid_sq_size / 2) &
-                                            (abs(centers_array[:, 1] - centers_array[i, 1]) <= grid_sq_size * 1.5))[0])
-            if len(below_fovs) > 0:
-                neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, below_fovs)][0]])
-
-            # Neighbor right
-            right_fovs = np.unique(np.where((centers_array[:, 0] > fovs.loc[fov, 'x_max']) &
-                                            (abs(centers_array[:, 1] - centers_array[i, 1]) <= grid_sq_size / 2) &
-                                            (abs(centers_array[:, 0] - centers_array[i, 0]) <= 400))[0])
-            if len(right_fovs) > 0:
-                neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, right_fovs)][0]])
-
-            # Neighbor left
-            left_fovs = np.unique(np.where((centers_array[:, 0] < fovs.loc[fov, 'x_min']) &
-                                           (abs(centers_array[:, 1] - centers_array[i, 1]) <= grid_sq_size / 2) &
-                                           (abs(centers_array[:, 0] - centers_array[i, 0]) <= 400))[0])
-            if len(left_fovs) > 0:
-                neighbors[i].append(fovs.index[euclidian_distances[np.isin(euclidian_distances, left_fovs)][0]])
-
-        fovs['neighbors'] = neighbors
-        return fovs
-
-    @staticmethod
     def get_fovs_dataframe(transcripts: pd.DataFrame):
         """
         Creates FOVs DataFrame including coordinates, transcript counts, z-ratio, neighbors
@@ -358,14 +369,14 @@ class Experiment:
         fovs : pd.DataFrame
             DataFrame of FOV coordinates, transcript counts, neighbors
         """
-        # Get coordinates, transcript counts, z-plane ratios
-        fovs = Experiment.find_fovs(transcripts)
+        # Get coordinates, transcript counts
+        fovs = find_fovs(transcripts)
         # Get cardinal neighbors
-        fovs = Experiment.get_fov_neighbors(fovs)
+        fovs = get_fov_neighbors(fovs)
+        # Get z-ratio per FOV
+        num_planes = transcripts['global_z'].nunique()
+        fovs['z_ratio'] = fovs[f'z_{num_planes - 1}_count'] - fovs['z0_count']
 
-        # Counts per gene
-        counts_per_gene = transcripts.groupby(['fov'])['gene'].value_counts().unstack(fill_value=0)
-        fovs = fovs.merge(counts_per_gene, left_index=True, right_index=True, how='left')
         return fovs
 
     @staticmethod
